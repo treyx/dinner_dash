138e0997fc0af779410b9f5b16a3bfc9dc619cdf
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-11 18:38:16 -0600

    created user and login routes also made the create method in the session controller to create a session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,16 @@ class SessionsController < ApplicationController
     
   end
 
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      flash[:notice] = "You are currently logged in as #{user.full_name}."
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash[:errors] = "Invalid Login"
+      render :new
+    end
+  end
+
 end
