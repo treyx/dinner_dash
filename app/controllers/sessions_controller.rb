debf850c10b7f30798928241fbe07c47aaf15b35
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-12 11:24:15 -0600

    need to work on flash notices... commented out for now

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,9 +8,9 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       if user.user?
-        redirect_to user_path(user) && flash[:notice] = "You are currently logged in as #{user.full_name}."
+        redirect_to user_path(user) # flash[:notice] = "You are currently logged in as #{user.full_name}."
       elsif user.admin?
-        redirect_to '/admin', notice: "Logged in."
+        redirect_to '/admin' # flash[:notice] "Logged in."
       end
     else
       flash[:errors] = "Invalid Login"
