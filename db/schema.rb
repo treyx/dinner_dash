912796c5145a1bbb125188609d8dd2e4a717eb78
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-12 10:39:01 -0600

    added default value for role and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150311235958) do
+ActiveRecord::Schema.define(version: 20150312163231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 20150311235958) do
     t.text    "password_digest"
     t.text    "email"
     t.text    "full_name"
-    t.integer "role"
+    t.integer "role",            default: 0
   end
 
 end
