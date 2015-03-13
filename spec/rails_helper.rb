41577769cfb5d56046cadc57b5bf90fb2427fe50
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-12 23:32:06 -0600

    moved db cleaner to spec helper... need to add simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,18 +9,17 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-  end
 
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
-  end
   
   config.use_transactional_fixtures = true
 
+  config.infer_spec_type_from_file_location!
+end
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
 end
