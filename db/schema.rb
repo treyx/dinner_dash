30216af757d931861e805eebcd6e5fa45ca4c222
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-13 15:47:54 -0600

    created migration to add default value for item status

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312163231) do
+ActiveRecord::Schema.define(version: 20150313214520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,9 +24,9 @@ ActiveRecord::Schema.define(version: 20150312163231) do
     t.text     "title"
     t.text     "description"
     t.integer  "price"
-    t.integer  "status"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer  "status",      default: 0
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
     t.integer  "category_id"
   end
 
