ad43dc845efb31018ca1a6e7953df3f3675aaf61
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-13 13:03:54 -0600

    modified tests for new factory and validations

--- spec/features/admin_manages_items_spec.rb
+++ spec/features/admin_manages_items_spec.rb
@@ -2,17 +2,17 @@ require "rails_helper"
 
 RSpec.feature "admin views the admin dashboard" do
   let(:admin) { User.create(email: "example@example.com", password: "password", role: 1) }
-  let(:item) { Item.create(title: "sushi") }
+  
   
   scenario "clicks on manage items" do
     admin
-    item
+    item = create(:item)
     visit "/login"
     fill_in("session[email]", with: admin.email)
     fill_in("session[password]", with: admin.password)
     click_link_or_button "Login"
     click_link_or_button "Manage Items"
-    expect(page).to have_content("sushi")
+    expect(page).to have_content("Sushi")
   end
 
   scenario "clicks on add item" do
--- spec/features/unauthenticated_users_feature_spec.rb
+++ spec/features/unauthenticated_users_feature_spec.rb
@@ -4,25 +4,25 @@ RSpec.feature "Unauthenticated user browses items" do
 
   context "unauthenticated user" do
     scenario "can browse the items" do
-      item = Item.create(title: "sushi")
+      item = create(:item)
       visit "/menu"
       expect(current_path).to eq(menu_path)
-      expect(page).to have_content("sushi")
+      expect(page).to have_content("Sushi")
     end
 
     scenario "can browse items in each category" do
       Category.create(id: 1, title: "appetizers")
-      item = Item.create(id: 1, title: "gyoza", category_id: 1 )
+      item = create(:item)
 
       visit "/categories/appetizers"
-      expect(page).to have_content("gyoza")
+      expect(page).to have_content("Sushi")
 
       Category.create(id: 2, title: "sashimi")
-      item2 = Item.create(id: 2, title: "hamachi", category_id: 2)
+      item2 = create(:item, title: "Hamachi", category_id: 2)
 
       visit "/categories/sashimi"
-      expect(page).to have_content("hamachi")
-      expect(page).to_not have_content("gyoza")
+      expect(page).to have_content("Hamachi")
+      expect(page).to_not have_content("Sushi")
     end
   end
 
