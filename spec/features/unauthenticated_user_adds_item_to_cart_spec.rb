c6cb39d0caea972d12613c987dd9d757a602fc02
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-14 14:58:18 -0600

    unskipped test and added some temporary pseudocode

--- spec/features/unauthenticated_user_adds_item_to_cart_spec.rb
+++ spec/features/unauthenticated_user_adds_item_to_cart_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.feature "Unauthenticated user" do
-  xscenario "adds item to cart" do
+  scenario "adds item to cart" do
     create(:item)
     visit "/menu"
     click_button("Add To Cart")
@@ -10,4 +10,10 @@ RSpec.feature "Unauthenticated user" do
     expect(page).to have_content "Sushi"
     expect(page).to have_content "1"
   end
+
+  #need to have test to automatically set quantity
+  #unless we decide to allow user to initially specifiy quantity
+
+  #if user adds item that already exists -> test that the quanity is increased and
+  #there is not a redundant entry in db
 end
