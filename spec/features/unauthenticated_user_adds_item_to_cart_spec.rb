a00b2f603706d046a4ebb06e93cb6f983f4fba5e
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-15 14:29:06 -0600

    modified spec and added test to display item quantity

--- spec/features/unauthenticated_user_adds_item_to_cart_spec.rb
+++ spec/features/unauthenticated_user_adds_item_to_cart_spec.rb
@@ -1,19 +1,28 @@
 require "rails_helper"
 
 RSpec.feature "Unauthenticated user" do
-  scenario "adds item to cart" do
-    create(:item)
-    visit "/menu"
-    click_button("Add To Cart")
-    click_on "View Cart"
-    puts current_path
-    within("#item_title") do
-      expect(page).to have_content "Sushi"
+  context "adds item to cart" do
+    scenario "and sees the item title" do
+      create(:item)
+      visit "/menu"
+      click_button("Add To Cart")
+      click_on "View Cart"
+      within("#item_title") do
+        expect(page).to have_content "Sushi"
+      end
+    end
+
+    scenario "and sees the item quantity" do
+      create(:item)
+      visit "/menu"
+      click_button("Add To Cart")
+      click_on "View Cart"
+      puts current_path
+      within("#item_quantity") do
+        expect(page).to have_content 1
+      end
     end
   end
-
-  #need to have test to automatically set quantity
-  #unless we decide to allow user to initially specifiy quantity
 
   #if user adds item that already exists -> 
   #test that the quanity is increased and
