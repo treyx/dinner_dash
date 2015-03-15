9331618d161befb4617f0c87111d46a074e1b2ef
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-15 13:22:23 -0600

    mainly adding these changes for lulz

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,13 @@
 class OrdersController < ApplicationController
   
   def create
+    binding.pry
     item = Item.find(params[:item_id])
-    @current_order.cart_items = {item.id => { :quantity => gs
-      1 } }
+    binding.pry
+    @current_order.cart_items = { item.id => { :quantity => 1 } }
+    binding.pry
     @current_order.save
     redirect_to menu_path, alert: "You are redirected yo!"
-  end
-
-  def show
-    binding.pry
-   @current_order
   end
 
 end
