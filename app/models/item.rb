18ce0c4ede40d72a342caa659d3ac7c85bc0784c
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-15 17:29:49 -0600

    some refactoring - removed unused ActiveReord associations in Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,6 @@
 class Item < ActiveRecord::Base
   validates :title, :description, :price, presence: true
-  has_many :orders, through: :order_items
-  has_many :order_items
+  has_many :orders
   has_many :categories
   enum status: %w(active retired)
 end
