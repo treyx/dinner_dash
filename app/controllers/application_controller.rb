d001e0313ee14f607aba6c9dbdc46c41b9616269
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-15 00:04:27 -0600

    before action necessary

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_order
 
   def create_cart
-    return @current_order if @current_order
-
-    @current_order = Order.find_or_create_by(id: session[:order_id])
-    session[:order_id] = @current_order.id
-    @current_order
+    @current_cart = Cart.new(session[:cart])
   end
   before_action :create_cart
+  helper_method :create_cart
 
-  def current_order
-    @current_order
-  end
-
+  #will need this
+  # def current_user
+  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  # end
+  # helper_method :current_user
 
 end
