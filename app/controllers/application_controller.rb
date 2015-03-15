410d5488a105b210a5d896677416f10d0fa44a62
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-15 13:26:37 -0600

    cart is created at start and assigned to session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :create_cart
+  before_action :create_cart
 
   def create_cart
     @current_cart = Cart.new(session[:cart])
   end
-  before_action :create_cart
-  helper_method :create_cart
-
-  #will need this
-  # def current_user
-  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  # end
-  # helper_method :current_user
-
 end
