4c025b8d557094f41b9d900c82275b5303280ad6
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-15 18:05:56 -0600

    added test for cart functionality

---
+++ spec/models/cart_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe Cart, type: :model do
+  context "an item is added to the cart" do
+    it "with valid attributes" do
+      item = create(:item)
+      cart = Cart.new(nil)
+      cart.add_item(1)
+      expect(cart.cart_items.count).to eq(1)
+      item2 = Item.create
+      cart.add_item(item2)
+      expect(cart.cart_items.count).to eq(2)
+    end
+  end
+end
