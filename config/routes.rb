0fcfb74f70725dac67472d67d1859d0376520edd
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-15 19:19:50 -0600

    hound fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
   root "cart#index"
-  
+
   get "/menu", to: "items#index"
 
   get "/cart", to: "cart#index"
   post "/cart", to: "cart#create"
   delete "/cart", to: "cart#delete"
-  
+
   resources :categories do
     resources :items
   end
-
-
 
   get "login" => "sessions#new", :as => "login"
   post "login" => "sessions#create"
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe Cart, type: :model do
   context "an item is added to the cart" do
     it "with valid attributes" do
-      item = create(:item)
+      create(:item)
       cart = Cart.new(nil)
       cart.add_item(1)
       expect(cart.cart_items.count).to eq(1)
