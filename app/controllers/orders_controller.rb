1e00ef9961339273a68de2657214c61727918447
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-17 13:11:23 -0600

    added index method to display current orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,8 +5,13 @@ class OrdersController < ApplicationController
     #   redirect_to login_path and return
     # end
     order = Order.create(status: "ordered",
-           cart_items: params[:cart])
+                     cart_items: params[:cart],
+                        user_id: current_user.id)
     flash[:notice] = "Order Submitted, Thank You!"
-      redirect_to root_path
+    redirect_to root_path
+  end
+
+  def index
+    @orders = current_user.orders
   end
 end
