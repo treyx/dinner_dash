35804b01a2f4da8e8baf4ac40f26225707996ce1
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-17 15:43:35 -0600

    added logic for formatting in view

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,18 @@
 class Order < ActiveRecord::Base
   belongs_to :user
 
+
+  def created_at_formatted
+    created_at.strftime("%A, %d %b %Y %l:%M %p")
+  end
+
+  def updated_at_formatted
+    updated_at.strftime("%A, %d %b %Y %l:%M %p")
+  end
+
+  def cart_item_and_quantity
+    items = Hash.new
+    cart_items.select { |item_id, quantity| items[Item.find(item_id)] = quantity }
+    items
+  end
 end
