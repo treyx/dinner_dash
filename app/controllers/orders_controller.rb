b1d70e17a475899432e1ddfa6bf096dd198e80ac
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-17 11:58:42 -0600

    refactoring

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,9 +4,8 @@ class OrdersController < ApplicationController
     #   flash[:error] = "You must log in to checkout. Please login."
     #   redirect_to login_path and return
     # end
-    submitted_cart = params[:cart]
     order = Order.create(status: "ordered",
-           cart_items: submitted_cart)
+           cart_items: params[:cart])
     flash[:notice] = "Order Submitted, Thank You!"
       redirect_to root_path
   end
