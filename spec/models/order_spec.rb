992cc48af9d45a934ccc5003aefc04aeb7aee4fa
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-17 21:16:27 -0600

    added basic model spec validations for order

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,4 +1,20 @@
 require "rails_helper"
 
 RSpec.describe Order, type: :model do
+  context "invalid attributes" do
+    it "is invalid without any attributes" do
+      order = Order.create
+      expect(order).to_not be_valid
+    end
+
+    it "is invalid without a user_id" do
+      order = Order.create(cart_items: "{'9'=>1}")
+      expect(order).to_not be_valid
+    end
+
+    it "is invalid without cart_items" do
+      order = Order.create(user_id: 1)
+      expect(order).to_not be_valid
+    end
+  end
 end
