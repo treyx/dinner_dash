5781832f3e41df9c4d44636636b2afc302dc275c
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-17 22:25:00 -0600

    added status back to orders with a default integer value of 0 - need to configure enum roles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150318041856) do
+ActiveRecord::Schema.define(version: 20150318042052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20150318041856) do
     t.hstore   "cart_items"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "status",     default: 0
   end
 
   create_table "users", force: :cascade do |t|
