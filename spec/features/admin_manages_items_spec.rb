c2f6b18fe4d5076b921b2786a35dfee967339b0b
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-17 23:00:05 -0600

    serious test refactoring - added test for admin managing item

--- spec/features/admin_manages_items_spec.rb
+++ spec/features/admin_manages_items_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.feature "admin views the admin dashboard" do
+RSpec.feature "a valid admin" do
   let(:admin) { User.create(email: "example@example.com", password: "password", full_name: "example", role: 1) }
 
   def add_new_item(title, description, price)
@@ -10,25 +10,36 @@ RSpec.feature "admin views the admin dashboard" do
     click_button 'Create Item'
   end
 
-  scenario "clicks on manage items" do
+  def admin_log_in
     admin
-    create(:item)
     visit "/login"
     fill_in("session[email]", with: admin.email)
     fill_in("session[password]", with: admin.password)
     click_link_or_button "Login"
-    click_link_or_button "Manage Items"
-    expect(page).to have_content("Sushi")
   end
 
-  scenario "clicks on add item" do
-    admin
-    visit "/login"
-    fill_in("session[email]", with: admin.email)
-    fill_in("session[password]", with: admin.password)
-    click_link_or_button "Login"
-    click_link_or_button "Add Item"
-    add_new_item "Sushi Roll", "A Roll Of Sushi", 8.45
-    expect(page).to have_content("Sushi Roll")
+  context "views the admin dashboard" do
+    scenario "clicks on manage items" do
+      admin_log_in
+      create(:item)
+      click_link_or_button "Manage Items"
+      expect(page).to have_content("Sushi")
+    end
+  
+    scenario "clicks on add item" do
+      admin_log_in
+      click_link_or_button "Add Item"
+      add_new_item "Sushi Roll", "A Roll Of Sushi", 8.45
+      expect(page).to have_content("Sushi Roll")
+    end
+  end
+  
+  context "can modify an item's attributes" do
+    scenario "makes an item retired" do
+      admin_log_in
+      create(:item)
+      click_link_or_button "Manage Items"
+      click_link_or_button "Sushi"
+    end
   end
 end
