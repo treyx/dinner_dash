626c26b969b17a379f2a69d16b089fe856723153
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-17 22:16:02 -0600

    removed price column from orders table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150315231516) do
+ActiveRecord::Schema.define(version: 20150318041407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 20150315231516) do
     t.text     "status"
     t.integer  "user_id"
     t.hstore   "cart_items"
-    t.string   "price",      array: true
     t.datetime "created_at"
     t.datetime "updated_at"
   end
