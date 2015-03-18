b760cc3a4f361c2fbc5709890083c96ce6dec4f3
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-17 21:39:05 -0600

    changed default order status

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,7 @@ class OrdersController < ApplicationController
     #   flash[:error] = "You must log in to checkout. Please login."
     #   redirect_to login_path and return
     # end
-    order = Order.create(status: "ordered",
+    order = Order.create(status: "Ordered",
                          cart_items: params[:cart],
                          user_id: current_user.id)
     flash[:notice] = "Order Submitted, Thank You!"
