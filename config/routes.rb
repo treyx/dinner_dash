4a924cd0106995ec172b88c535db4eaa387c142e
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-19 10:20:31 -0600

    removed comment

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,6 @@ Rails.application.routes.draw do
   namespace 'admin' do
     get '/', to: 'dashboard#index', as: '/'
     resources :items
-    # get '/items/retire/:id', to: 'items#retire'
     resources :categories
   end
 
