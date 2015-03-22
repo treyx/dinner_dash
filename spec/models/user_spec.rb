054968dad94f14933ad4c0a53b06c2a331352844
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-21 19:32:07 -0600

    added validation for valid user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,14 @@
 require "rails_helper"
 
 RSpec.describe User, :type => :model do
-  context "invalid attributes" do
+  context "valid attributes" do
+    it "is valid" do
+      user = User.create(email: "example@example.com", password: "password", full_name: "example")
+      expect(user).to be_valid
+    end
+  end
 
+  context "invalid attributes" do
     it "is invalid without a password" do
       user = User.new(email: "example@example.com")
       expect(user).to_not be_valid
